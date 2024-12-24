f1749015ca8523a9ef60419b837cb1ca8c5914ff
HARIKRISHNA95504 <patimaharikrishnap@gmail.com>
2024-12-24 08:07:42 +0530

    created Navbar and updated products

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -14,15 +14,163 @@ function Products(){
                 "Exynos 1330 Processor",
                 "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
             ]
-        }
+        },
+        {
+            id:2,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/y/8/i/-original-imah4zp7fgtezhsz.jpeg?q=70",
+            name:"SAMSUNG Galaxy S23 5G (Cream, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:37999,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:3,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/c/n/9/-original-imah2qyrg2xryu8m.jpeg?q=70",
+            name:"POCO C61  - Locked with Airtel Prepaid (Mystical Green, 64 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:12000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:4,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/i/r/t3-lite-5g-v2356-vivo-original-imah27wmfgkergtx.jpeg?q=70",
+            name:"vivo T3 Lite 5G (Vibrant Green, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:15000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:5,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/b/y/g/-original-imah4qtgjtzprg3g.jpeg?q=70",
+            name:"Motorola G85 5G (Viva Magenta, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:17999,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:6,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/j/z/3/-original-imagtc5fqyz8tu4c.jpeg?q=70",
+            name:"Apple iPhone 15 Plus (Green, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:62500,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:7,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/q/n/s/-original-imah4jyfrgsbebg9.jpeg?q=70",
+            name:"Apple iPhone 16 (Ultramarine, 256 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:89000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:8,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/p/t/-original-imahyzygjdzrjgfh.jpeg?q=70",
+            name:"Google Pixel 8a (Aloe, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:59000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:9,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/e/3/-original-imah37gwn2xbvzhy.jpeg?q=70",
+            name:"OPPO K12x 5G with 45W SUPERVOOC Charger In-The-Box (Breeze Blue, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:48000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
+        {
+            id:10,
+            imgSrc:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/z/j/w/-original-imah4d9fkzjzmfxv.jpeg?q=70",
+            name:"OPPO A3 5G (Nebula Red, 128 GB)",
+            rating:"62,060 Ratings & 2,959 Reviews",
+            price:28000,
+            specifications:[
+                "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
+                "16.76 cm (6.6 inch) Full HD+ Display",
+                "50MP + 2MP + 2MP | 13MP Front Camera",
+                "5000 mAh Lithium Ion Battery",
+                "Exynos 1330 Processor",
+                "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-Box Accessories"
+            ]
+        },
         
     ]
     const AddToCart = (productsInfo)=>{
         console.log('it is addToCart function...',productsInfo)
     }
     return(
-        <div className="container font-sans border-b-2 pb-3" >
-            <div className="flex justify-between pt-5 " >
+        
+        <div className="container font-sans  pb-3" >
+            <div className="bg-blue-600 flex justify-center gap-5 text-center items-center ">
+                <div>
+                <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png" className=" pt-1 pb-1"/>
+                </div>
+                <div className="bg-white w-2/4">
+                    <input type="text" placeholder="Serach for Products,Brands and More" className="border-none rounded w-full pl-1  py-1"/>
+                </div>
+                <button className=" bg-white text-blue-600 text-lg font-semibold px-9">Login</button>
+                <p className="text-white cursor-pointer ">Become a seller</p>
+                <p className="text-white cursor-pointer ">More</p>
+                <p className="text-white cursor-pointer">Cart</p>
+            </div>
+            <div className="flex justify-between pt-5 border-b-2 pb-2 " >
                 <div className="w-1/4 pl-5 ">
                     <img src={products[0].imgSrc}/>
                 </div>
@@ -39,6 +187,66 @@ function Products(){
                 </div>
                 <div className="w-1/4">
                     <p className="font-semibold ">{products[0].price}</p>
+                    <button className="rounded bg-blue-600 text-white font-semibold text-lg px-3 py-1 mt-2" onClick={()=>AddToCart(products)}>AddtoCart</button>
+                </div>
+            </div>
+            <div className="flex justify-between pt-5 border-b-2 pb-2" >
+                <div className="w-1/4 pl-5 ">
+                    <img src={products[1].imgSrc}/>
+                </div>
+                <div className="w-2/4">
+                    <p className="font-semibold text-lg ">{products[1].name}</p>
+                    <p className="text-gray-400 font-semibold text-lg"><span className="rounded text-white bg-green-500 px-2.5 mr-2">4.2</span>{products[1].rating}</p>
+                    <ul className=" pt-4">
+                        {products[1].specifications.map(specification =>(
+                            <li key={specification}>{specification}</li>
+
+                        ))}
+
+                    </ul>
+                </div>
+                <div className="w-1/4">
+                    <p className="font-semibold ">{products[1].price}</p>
+                    <button className="rounded bg-blue-600 text-white font-semibold text-lg px-3 py-1 mt-2" onClick={()=>AddToCart(products)}>AddtoCart</button>
+                </div>
+            </div>
+            <div className="flex justify-between pt-5 border-b-2 pb-2 " >
+                <div className="w-1/4 pl-5 ">
+                    <img src={products[2].imgSrc}/>
+                </div>
+                <div className="w-2/4">
+                    <p className="font-semibold text-lg ">{products[2].name}</p>
+                    <p className="text-gray-400 font-semibold text-lg"><span className="rounded text-white bg-green-500 px-2.5 mr-2">4.2</span>{products[2].rating}</p>
+                    <ul className=" pt-4">
+                        {products[2].specifications.map(specification =>(
+                            <li key={specification}>{specification}</li>
+
+                        ))}
+
+                    </ul>
+                </div>
+                <div className="w-1/4">
+                    <p className="font-semibold ">{products[2].price}</p>
+                    <button className="rounded bg-blue-600 text-white font-semibold text-lg px-3 py-1 mt-2" onClick={()=>AddToCart(products)}>AddtoCart</button>
+                </div>
+            </div>
+            <div className="flex justify-between pt-5 border-b-2 pb-2 " >
+                <div className="w-1/4 pl-5 ">
+                    <img src={products[3].imgSrc}/>
+                </div>
+                <div className="w-2/4">
+                    <p className="font-semibold text-lg ">{products[3].name}</p>
+                    <p className="text-gray-400 font-semibold text-lg"><span className="rounded text-white bg-green-500 px-2.5 mr-2">4.2</span>{products[3].rating}</p>
+                    <ul className=" pt-4">
+                        {products[3].specifications.map(specification =>(
+                            <li key={specification}>{specification}</li>
+
+                        ))}
+
+                    </ul>
+                </div>
+                <div className="w-1/4">
+                    <p className="font-semibold ">{products[3].price}</p>
                     <button className="rounded bg-blue-600 text-white font-semibold text-lg px-3 py-1 mt-2" onClick={()=>AddToCart(products)}>AddtoCart</button>
                 </div>
             </div>
